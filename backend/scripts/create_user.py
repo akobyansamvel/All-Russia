f1bfd8457f736c20a7b21170070bf0494a5c5df4
nsstnc <egorgolubev0484@gmail.com>
2024-10-09 19:00:49 +0300

    Deleted backend parts

--- backend/scripts/create_user.py
+++
@@ -1,6 +0,0 @@
-from databases import SQLiteDatabase
-import json, pathlib
-database = SQLiteDatabase(f"{str(pathlib.Path(__file__).parent.resolve())}/database.db")
-database.connect()
-# добавление пользователя админа
-database.create_user('admin', 'admin')
